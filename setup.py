7c37d24ac13e4bde3e9d10de90ab954df882a36c
Chris <christopheralexman@gmail.com>
2021-07-17 11:50:43 -0700

    Add project setup, idle and rfid frames
    * Add the structural organization of the project with recommended files
    * Add to gui/ the idle and scan rfid card frames, not completed
    * Add directories for unit tests, data, images, and pump / rfid code

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CS179J-Smart-Water-Station',
+    version='0.1',
+    packages=find_packages(),
+    description='UCR Summer 2021 CS179J Senior Design Project'
+)
