7943f5171862e9bc795fa2d0a5462f048be6018c
idelc <idel012@ucr.edu>
2021-08-02 13:35:15 -0700

    Proper pump function format

--- pumpScript.py
+++ pumpScript.py
@@ -5,12 +5,14 @@ import time
 # OutputDevice is a generic device, which we can use as the basis
 # for a new class, which we will call "relay" and use to control it
 
+#commented lines are for testing purposes
+
 class Relay(OutputDevice):
 	def __init__(self, pin, active_high):
 		super(Relay, self).__init__(pin, active_high)
 
 def pumpActive(): 
-	print("Running")
+#	print("Running")
 	button = Button(14)
 	led = LED(15)
 	rly = Relay(12,False)
@@ -23,6 +25,13 @@ def pumpActive():
 	led.off()
 	rly.off()
 	btnPressTime = time.time() - pushed
-	print 'Button was pressed for %.2f seconds' % btnPressTime
+#	print 'Button was pressed for %.2f seconds' % btnPressTime
 	return btnPressTime * 0.0275 # approximate calculation
 
+#def main():
+#	wtr = pumpActive()
+#	print 'water consumed %.2f L' % wtr
+#	exit()
+
+#if __name__ == "__main__":
+#    main()
