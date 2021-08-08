8c005e0584631f9d7a8e1f8f3df2468069e4c837
Chris <christopheralexman@gmail.com>
2021-08-07 20:22:30 -0700

    Add version 1.0 and change description to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='CS179J-Smart-Water-Station',
-    version='0.1',
+    version='1.0',
     packages=['gui', 'rfid', 'pump'],
-    description='UCR Summer 2021 CS179J Senior Design Project'
+    description='UC Riverside Summer 2021 CS179J Senior Design Project'
 )
